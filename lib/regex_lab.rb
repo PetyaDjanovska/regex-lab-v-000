b25a7576cf7a68c47818d5a7ddfb8d228dc47f0d
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-10 19:39:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aAeEoOuUiI]/)? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split(" ").grep(/^\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
